5a3541df60602509c7abd03e6234e0652937f0ec
Matheus Oscar Ferreira de Souza <matheus.oscar@webmanagers.com.br>
2023-09-01 08:09:01 -0300

    Começando insert

--- LingoMax/LingoMax.cpp
+++ LingoMax/LingoMax.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <locale>
 
 using namespace std;
 
@@ -50,12 +51,35 @@ struct Usuario_index {
 };
 
 void insert_idioma(Idioma table[], Idioma_index index[], int n, int len[]) {
-
+	system("clear||cls");
+	char finalizar[1];
+	cout << "========= CADASTRAR IDIOMA =========" << endl;
+	for (int i = 0; i < n; i++) {
+		cout << "Código: ";
+		cin >> table[n].cod_idioma;
+		cout << "Descrição: ";
+		cin.ignore();
+		cin.getline(table[i].descricao, 30);
+		len[0]++;
+		cout << "===========================================" << endl;
+		cout << "Deseja cadastrar outro idioma?[S][N] ";
+		cin >> finalizar;
+		if (strcmp(finalizar, "N") == 0 || strcmp(finalizar, "n") == 0) {
+			return;
+		}
+		else {
+			insert_idioma(table, index, n, len);
+			return;
+		}
+	}
 }
 
+
 int main() {
+	setlocale(LC_ALL, "portuguese");
 	const int n = 1000;
 	int len[4];
+	int opcao = 0;
 	/*
 		len[0] -> Idioma
 		len[1] -> Licao
@@ -64,4 +88,29 @@ int main() {
 	*/
 	Idioma idiomas[n];
 	Idioma_index idiomas_index[n];
+
+	while (true) {
+		system("clear||cls");
+		cout << "---------- MENU ----------" << endl;
+		cout << "CADASTROS        [1]" << endl;
+		cout << "DIGITE UMA OPCAO: ";
+		cin >> opcao;
+		switch (opcao) {
+		case 1:
+			system("clear||cls");
+			cout << "---------- CADASTROS ----------" << endl;
+			cout << "CADASTRAR IDIOMA [1]" << endl;
+			cout << "DIGITE UMA OPÇÃO DE CADASTRO (0 parar sair): ";
+			cin >> opcao;
+			switch (opcao) {
+			case 1:
+				insert_idioma(idiomas, idiomas_index, n, len);
+				break;
+			case 0:
+				break;
+			}
+			break;
+		}
+	};
+	
 }
