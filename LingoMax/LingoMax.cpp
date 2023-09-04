4dfc7433e3d987846c1f2cfd131ebb2e8bae345c
Matheus Oscar Ferreira de Souza <matheus.oscar@webmanagers.com.br>
2023-09-04 11:11:36 -0300

    Finalizado inserção

--- LingoMax/LingoMax.cpp
+++ LingoMax/LingoMax.cpp
@@ -54,33 +54,16 @@ struct Usuario_index {
 	int end;
 };
 
+/*================================== INICIO INSERIR DADOS ==================================*/
 
 void register_index_idioma(Idioma table[], Idioma_index index[], int len[], int end) {
-	if (len[0] - 1 == 0) {
-		index[0].cod = table[0].cod_idioma;
-		index[0].end = end;
-	}
-	else {
-		int i = 0, f = len[0] - 1;
-		int m = (i + f) / 2;
-		for (; f >= i; m = (i + f) / 2) {
-			cout << "m: " << m << endl;
-			cout << "index: " << index[m].cod << endl;
-			cout << "table: " << table[end].cod_idioma << endl;
-			if (table[end].cod_idioma > index[m].cod)
-				i = m + 1;
-			else
-				f = m - 1;
-		}
-
-		for (int j = len[0] - 1; j >= i; --j) {
-			index[j + 1] = index[j];
-		}
-		index[m].cod = table[end].cod_idioma;
-		index[m].end = end;
-		cout << m << endl;
-	};
-
+	int fim = len[0] - 1;
+	for (; fim >= 0 && index[fim].cod > table[end].cod_idioma; fim--) {
+        index[fim + 1] = index[fim];
+    }
+	index[fim + 1].cod = table[end].cod_idioma;
+	index[fim + 1].end = end;
+}
 
 
 void insert_idioma(Idioma table[], Idioma_index index[], int n, int len[]) {
@@ -94,8 +77,8 @@ void insert_idioma(Idioma table[], Idioma_index index[], int n, int len[]) {
 		cin.ignore();
 		cin.getline(table[i].descricao, 30);
 		table[i].deletado = false;
-		len[0]++;
 		register_index_idioma(table, index, len, i);
+		len[0]++;
 		cout << "===========================================" << endl;
 		cout << "Deseja cadastrar outro idioma?[S][N] ";
 		cin >> finalizar;
@@ -109,6 +92,126 @@ void insert_idioma(Idioma table[], Idioma_index index[], int n, int len[]) {
 	}
 }
 
+void register_index_licao(Licao table[], Licao_index index[], int len[], int end) {
+	int fim = len[1] - 1;
+	for (; fim >= 0 && index[fim].cod > table[end].cod_licao; fim--) {
+		index[fim + 1] = index[fim];
+	}
+	index[fim + 1].cod = table[end].cod_licao;
+	index[fim + 1].end = end;
+}
+
+void insert_licao(Licao table[], Licao_index index[], int n, int len[]) {
+	system("clear||cls");
+	char finalizar[2];
+	cout << "========= CADASTRAR LIÇÃO =========" << endl;
+	for (int i = len[1]; i < n; i++) {
+		cout << "Código: ";
+		cin >> table[i].cod_licao;
+		cout << "Código idioma: ";
+		cin >> table[i].cod_idioma;
+		cout << "Total niveis: ";
+		cin >> table[i].total_niveis;
+		table[i].deletado = false;
+		register_index_licao(table, index, len, i);
+		len[1]++;
+		cout << "===========================================" << endl;
+		cout << "Deseja cadastrar outro lição?[S][N] ";
+		cin >> finalizar;
+		if (strcmp(finalizar, "N") == 0 || strcmp(finalizar, "n") == 0) {
+			return;
+		}
+		else {
+			insert_licao(table, index, n, len);
+			return;
+		}
+	}
+}
+
+
+void register_index_exercicio(Exercicio table[], Exercicio_index index[], int len[], int end) {
+	int fim = len[2] - 1;
+	for (; fim >= 0 && index[fim].cod > table[end].cod_exercicio; fim--) {
+		index[fim + 1] = index[fim];
+	}
+	index[fim + 1].cod = table[end].cod_exercicio;
+	index[fim + 1].end = end;
+}
+
+void insert_exercicio(Exercicio table[], Exercicio_index index[], int n, int len[]) {
+	system("clear||cls");
+	char finalizar[2];
+	cout << "========= CADASTRAR EXERCICIO =========" << endl;
+	for (int i = len[2]; i < n; i++) {
+		cout << "Código: ";
+		cin >> table[i].cod_exercicio;
+		cout << "Nivel de dificuldade: ";
+		cin >> table[i].nivel_dificuldade;
+		cout << "Pergunta: ";
+		cin.ignore();
+		cin.getline(table[i].pergunta, 120);
+		cout << "Resposta: ";
+		cin.ignore();
+		cin.getline(table[i].resposta_correta, 100);
+		cout << "Pontos: ";
+		cin >> table[i].pontos;
+		table[i].deletado = false;
+		register_index_exercicio(table, index, len, i);
+		len[2]++;
+		cout << "===========================================" << endl;
+		cout << "Deseja cadastrar outro exercicio?[S][N] ";
+		cin >> finalizar;
+		if (strcmp(finalizar, "N") == 0 || strcmp(finalizar, "n") == 0) {
+			return;
+		}
+		else {
+			insert_exercicio(table, index, n, len);
+			return;
+		}
+	}
+}
+
+void register_index_usuario(Usuario table[], Usuario_index index[], int len[], int end) {
+	int fim = len[3] - 1;
+	for (; fim >= 0 && index[fim].cod > table[end].cod_usuario; fim--) {
+		index[fim + 1] = index[fim];
+	}
+	index[fim + 1].cod = table[end].cod_usuario;
+	index[fim + 1].end = end;
+}
+
+void insert_usuario(Usuario table[], Usuario_index index[], int n, int len[]) {
+	system("clear||cls");
+	char finalizar[2];
+	cout << "========= CADASTRAR USUARIO =========" << endl;
+	for (int i = len[3]; i < n; i++) {
+		cout << "Código: ";
+		cin >> table[i].cod_usuario;
+		cout << "Nome: ";
+		cin.ignore();
+		cin.getline(table[i].nome, 60);
+		cout << "Código idioma: ";
+		cin >> table[i].cod_idioma;
+		table[i].nivel_atual = 0;
+		table[i].pontuacao_total = 0;
+
+		table[i].deletado = false;
+		register_index_usuario(table, index, len, i);
+		len[3]++;
+		cout << "===========================================" << endl;
+		cout << "Deseja cadastrar outro usuario?[S][N] ";
+		cin >> finalizar;
+		if (strcmp(finalizar, "N") == 0 || strcmp(finalizar, "n") == 0) {
+			return;
+		}
+		else {
+			insert_usuario(table, index, n, len);
+			return;
+		}
+	}
+}
+
+/*================================== FIM INSERIR DADOS ==================================*/
 
 
 int main() {
@@ -125,22 +228,43 @@ int main() {
 	Idioma idiomas[n];
 	Idioma_index idiomas_index[n];
 
+	Licao licoes[n];
+	Licao_index licoes_index[n];
+
+	Exercicio exercicios[n];
+	Exercicio_index exercicios_index[n];
+
+	Usuario usuarios[n];
+	Usuario_index usuarios_index[n];
+
 	while (true) {
 		system("clear||cls");
 		cout << "---------- MENU ----------" << endl;
 		cout << "CADASTROS        [1]" << endl;
-		cout << "DIGITE UMA OPCAO: ";
+		cout << "DIGITE UMA OPÇÃO: ";
 		cin >> opcao;
 		switch (opcao) {
 		case 1:
 			system("clear||cls");
 			cout << "---------- CADASTROS ----------" << endl;
-			cout << "CADASTRAR IDIOMA [1]" << endl;
+			cout << "CADASTRAR IDIOMA    [1]" << endl;
+			cout << "CADASTRAR LIÇÃO     [2]" << endl;
+			cout << "CADASTRAR EXERCICIO [3]" << endl;
+			cout << "CADASTRAR USUARIO   [4]" << endl;
 			cout << "DIGITE UMA OPÇÃO DE CADASTRO (0 parar sair): ";
 			cin >> opcao;
 			switch (opcao) {
 			case 1:
 				insert_idioma(idiomas, idiomas_index, n, len);
+				break;
+			case 2:
+				insert_licao(licoes, licoes_index, n, len);
+				break;
+			case 3:
+				insert_exercicio(exercicios, exercicios_index, n, len);
+				break;
+			case 4:
+				insert_usuario(usuarios, usuarios_index, n, len);
 				break;
 			case 0:
 				break;
